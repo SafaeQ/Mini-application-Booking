597a4fc9cd4237eea738693d40aef0a7023548a2
SafaeQ <safaedani009@gmail.com>
2022-01-18 17:32:59 +0100

    connect with mongodb

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const router = require('./routes')
 const express = require('express');
+const {
+    mongoClient
+} = require('mongodb')
 const app = express();
 const port = 8080;
 
@@ -9,6 +12,11 @@ const http = require('http').createServer(app);
 // start the server at port 8080
 http.listen(port, () => {
     console.log(`The server is listening on port http://localhost:${port}`)
+
+    mongoClient.connect('mongodb://localhost:8080', (err, client) => {
+        let dbName = client.db('Booking App');
+        console.log(` My Mongo Database ${dbName} is running `)
+    })
 });
 
 app.use('/', router)
