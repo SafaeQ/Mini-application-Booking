8c40404c82b8891e0de207d6e6e9868751c81921
SafaeQ <safaedani009@gmail.com>
2022-01-18 16:55:22 +0100

    // start the server at port

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 8080;
 const http = require('http').createServer(app);
 
+
+// start the server at port 8080
 http.listen(port, () => {
     console.log(`The server is listening on port http://localhost:${port}`)
 });
