60a74610383a148263d06e444b31fdb700d4bdee
SafaeQ <safaedani009@gmail.com>
2022-01-18 21:15:21 +0100

    export sign up

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,3 +2,23 @@ const {
     User,
     Role
 } = require('../models')
+const jwt = require('jsonwebtoken')
+
+const signup = (req, res, next) => {
+    const user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+
+    user.save((err, user) => {
+        if (err) {
+            res.status(500).send(err.message)
+            return;
+        }
+    })
+}
+
+module.exports = {
+    signup
+}
