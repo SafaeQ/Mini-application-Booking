7cfef076c9b0a686e25cbea85887888f2ae73b1f
SafaeQ <safaedani009@gmail.com>
2022-01-19 12:05:46 +0100

    Connect to Mongoose and set connection variable

--- server.js
+++ server.js
@@ -9,22 +9,20 @@ const port = 8080;
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+app.use(bodyParser.json)
 const http = require('http').createServer(app);
 
+mongoose.connect('mongodb://127.0.0.1:27017', {
+    useNewUrlParser: true
+});
 
+let db = mongoose.Connection;
 // start the server at port 8080
 http.listen(port, () => {
     console.log(`The server is listening on port http://localhost:${port}`)
-    con = 'mongodb://127.0.0.1:27017'
-    MongoClient.connect(con, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }, (err, client) => {
-        if (err) throw err
 
-        const db = client.db('Booking_App');
-        console.log(` My Mongo Database Booking_App is running `)
-    })
+    console.log(` My Mongo Database Booking_App is running `)
+
 });
 
 app.use('/', router)
