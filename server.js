c549e382f0b361d10802b6d5601749dcc6b674f5
SafaeQ <safaedani009@gmail.com>
2022-01-20 22:05:12 +0100

    edit

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const router = require('./routes')
 const express = require('express');
 let bodyParser = require('body-parser')
 
-// let mongoose = require('mongoose')
-// mongoose.connect('mongodb://127.0.0.1:27017', {
-//     useNewUrlParser: true
-// });
 
 const app = express();
 const port = 8080;
