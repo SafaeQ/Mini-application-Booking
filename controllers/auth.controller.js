cc84d6ec1f5c33e932ff02da08bf2d3f2c4c46b4
SafaeQ <safaedani009@gmail.com>
2022-01-21 19:52:06 +0100

    solve err

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const {
     models: {
-        User
+        User,
+        Role
     }
 } = require('../models')
 const bcrypt = require('bcryptjs')
@@ -90,7 +91,34 @@ const signin = async (req, res) => {
     }
 }
 
+
+function initial() {
+    Role.countDocuments({}, (err, count) => {
+        if (!err && count === 0) {
+            new Role({
+                name: 'admin'
+            }).save(err => {
+                if (err) throw err
+                console.log('add "admin" to roles');
+            });
+            new Role({
+                name: 'user'
+            }).save(err => {
+                if (err) throw err
+                console.log('add "user" to roles');
+            })
+            new Role({
+                name: 'particular'
+            }).save(err => {
+                if (err) throw err
+                console.log('add "particular" to roles')
+            })
+        }
+    })
+}
+
 module.exports = {
     signup,
-    signin
+    signin,
+    initial
 }
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = require('./routes')
 let bodyParser = require('body-parser')
-
+const initial = require('./controllers/auth.controller')
 
 const app = express();
 const port = 8080;
@@ -19,35 +19,5 @@ app.use('/', router)
 // start the server at port 8080
 app.listen(port, () => {
     console.log(`The server is listening on port http://localhost:${port}`)
-    initial()
+    initial
 });
-
-
-// Initialize
-const db = require('./models')
-const Role = db.role;
-
-function initial() {
-    Role.count((err, count) => {
-        if (!err && count === 0) {
-            new Role({
-                name: 'admin'
-            }).save(err => {
-                if (err) throw err
-                console.log('add "admin" to roles');
-            });
-            new Role({
-                name: 'user'
-            }).save(err => {
-                if (err) throw err
-                console.log('add "user" to roles');
-            })
-            new Role({
-                name: 'particular'
-            }).save(err => {
-                if (err) throw err
-                console.log('add "particular" to roles')
-            })
-        }
-    })
-}
