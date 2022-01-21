f3f1a59be9d225b94477513eb96e5d5da8175d6f
SafaeQ <safaedani009@gmail.com>
2022-01-21 20:46:42 +0100

    expressLayouts

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const router = require('./routes')
 let bodyParser = require('body-parser')
-
+const expressLayouts = require('express-ejs-layouts')
 const app = express();
 const port = 8080;
+const router = require('./routes')
 
 //  parses urlencoded bodies and only looks at requests
 app.use(bodyParser.urlencoded({
