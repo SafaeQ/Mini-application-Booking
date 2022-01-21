d9796379ee22cfb1229d36481e6ea1d2f09b5687
SafaeQ <safaedani009@gmail.com>
2022-01-21 23:28:26 +0100

    initial

---
+++ controllers/role.controller.js
@@ -0,0 +1,33 @@
+const {
+    models: {
+        Role
+    }
+} = require('../models')
+
+function initial() {
+    Role.countDocuments((err, count) => {
+        if (!err && count === 0) {
+            new Role({
+                name: 'admin'
+            }).save(err => {
+                if (err) throw err
+                console.log('add "admin" to roles');
+            });
+            new Role({
+                name: 'user'
+            }).save(err => {
+                if (err) throw err
+                console.log('add "user" to roles');
+            })
+            new Role({
+                name: 'particular'
+            }).save(err => {
+                if (err) throw err
+                console.log('add "particular" to roles')
+            })
+        }
+    })
+}
+module.exports = {
+    initial
+}
