92ea5b41be210efb992ba6ae6d10b21ce401565b
SafaeQ <safaedani009@gmail.com>
2022-01-22 16:23:56 +0100

    signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,53 +6,70 @@ const {
 } = require('../models')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const {
+    userToken
+} = require('../utils/token')
 
 const signup = async (req, res, next) => {
 
-    try {
-        // get user 
-        const {
-            username,
-            email,
-            password,
-            roles
-        } = req.body;
-        // validation 
-        if (!(email && password && username && roles)) {
-            res.status(400)
-        }
+    // get user 
+    const {
+        username,
+        email,
+        password,
+        roles
+    } = req.body;
 
+    // validation 
+    if (!(email || !password)) {
+        return res.status(400).send({
+            error: 'Please full all fields and try again'
+        })
+    }
+
+    // check if there is any user already in db
+    // ****************************
+    // const checkExistingUser = await User.find({
+    //     where: {
+    //         email: email,
+    //     }
+    // })
+
+    // if (checkExistingUser) {
+    //     return res.status(400).send({
+    //         error: 'hey u this is not ur email...'
+    //     })
+    // }
+
+    try {
         // encrypt password
         let encryptedPassword = await bcrypt.hash(password, 10)
 
         // create user in db
         const user = await User.create({
-            username,
-            email: email.toLowerCase(),
-            password: encryptedPassword,
-            // roles
-        })
+                username,
+                email: email.toLowerCase(),
+                password: encryptedPassword,
+                roles
+            })
+            .catch((err) => {
+                throw err
+            })
 
-        // Create token
-        const token = jwt.sign({
-                user_id: user._id,
-                email: user.email,
-                password: user.id,
-                // roles: user.roles
-            },
-            "secret", {
-                expiresIn: "1h",
-            }
-        );
+        console.log(user)
+        return res.json({
+            token: userToken(user)
+        })
 
         // save user token
         user.token = token;
+
         // return new user
         res.send(user);
         // res.status(200).json(user)
 
     } catch (error) {
-        console.log(error)
+        console.error(error)
     }
     // res.render('auth/signup.ejs')
 }
