73f6d8b9cbeb70df479db84903156992a2a1dd49
SafaeQ <safaedani009@gmail.com>
2022-01-22 15:26:25 +0100

    create token

---
+++ utils/token.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+
+// Create token
+const userToken = (user) => {
+    const timestamp = new Date().getTime()
+    return jwt.sign({
+        user_id: user._id,
+        email: user.email,
+        at: timestamp,
+        role: user.role
+
+    }, 'secret')
+}
+
+module.exports = {
+    userToken
+}
