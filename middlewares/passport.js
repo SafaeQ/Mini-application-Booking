6ebcff2cca1d816a7f56fa878dce0b6a797e34e4
SafaeQ <safaedani009@gmail.com>
2022-01-23 14:04:00 +0100

    setting the passport of middleware!

---
+++ middlewares/passport.js
@@ -0,0 +1,35 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const {
+    models: {
+        User
+    }
+} = require('../models')
+
+passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        session: false
+    },
+    async (email, password, done) => {
+        try {
+            const user = await User.findOne({
+                where: {
+                    email: email
+                }
+
+            })
+            console.log(user);
+            if (err) return done(err);
+            if (!user) return done(null, false, {
+                message: 'incorrect email'
+            })
+            // if (!user.verifyPassword(password)) return done(null, false)
+            return done(null, user)
+
+        } catch (error) {
+            console.error(error)
+        }
+
+    }
+));
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const authRouter = express.Router() //create new obj of router
+const passport = require('passport');
+require('../middlewares/passport')
 
 // import functions of users from controller
 const {
@@ -9,13 +11,20 @@ const {
 
 // console.log(auth_signup);
 
+// passport
+const requireSignIn = passport.authenticate('local', {
+    session: false,
+    // successRedirect: '/views/test',
+    // failureRedirect: '/',
+})
+
 // my api for auth
 authRouter.get('/signup', (req, res) => {
     // console.log('fd');
     // res.send('hello')
     res.render('auth/signup')
 })
-authRouter.post('/signup', auth_signup)
+authRouter.post('/signup', requireSignIn, auth_signup)
 authRouter.post('/login', auth_login)
 
 module.exports = authRouter
