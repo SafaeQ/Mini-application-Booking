18e660f90fbb22e9386a3b057cb53e06261a8fb0
SafaeQ <safaedani009@gmail.com>
2022-01-26 22:08:35 +0100

    set up !

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -15,3 +15,20 @@ const getAllUsers = async (req, res) => {
         console.error(error)
     }
 }
+
+const get_single_user = async (req, res) => {
+    const id = req.params.id
+    try {
+        const user = await User.findById(id)
+            .catch((err) => {
+                throw err
+            })
+        res.status(200).send(user)
+    } catch (error) {
+        console.log(error);
+    }
+}
+module.exports = {
+    getAllUsers,
+    get_single_user
+}
