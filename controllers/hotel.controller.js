58eba917a384a05d4e5a901739d55bf16603c756
SafaeQ <safaedani009@gmail.com>
2022-01-26 13:51:54 +0100

    crud function!

--- controllers/hotel.controller.js
+++ controllers/hotel.controller.js
@@ -50,7 +50,69 @@ const get_single_Hotel = async (req, res) => {
     }
 }
 
+const create_hotel = async (req, res) => {
+    const {
+        id,
+        name,
+        ville,
+        date,
+        stars,
+        image,
+        adress
+    } = req.body;
+    try {
+        const result = await Hotel
+            .create({
+                data: {
+                    id,
+                    name,
+                    ville,
+                    date,
+                    stars,
+                    image,
+                    adress
+                },
+            })
+            .catch((err) => {
+                throw err;
+            })
+        res.send(result);
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const remove_hotel = async (req, res) => {
+    const {
+        id
+    } = req.body;
+    try {
+        const result = await Hotel
+            .delete({
+                where: {
+                    id: _id
+                }
+            })
+            .catch((error) => {
+                throw error
+            })
+        if (result.count === 1) {
+            res.status(200).json('Your Hotel has been removed successfully')
+        } else {
+            res.status(200).json('no hotel found!')
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const update_hotel = async (req, res) => {
+
+}
 module.exports = {
     getAllHotels,
-    get_single_Hotel
+    get_single_Hotel,
+    create_hotel,
+    remove_hotel,
+    update_hotel
 }
