863603c0b38eb15f6a905611e2c801be686153da
SafaeQ <safaedani009@gmail.com>
2022-02-03 09:25:39 +0100

    ok

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -1,7 +1,24 @@
 const jwt = require('jsonwebtoken')
+const {
+    models: {
+        User
+    }
+} = require('../models')
 
 const isAuth = (req, res, next) => {
-
+    User.findById(id.session.user_id).exec(function (err, user) {
+        if (err) {
+            return next(err)
+        } else {
+            if (user === null) {
+                let err = new Error('not authorized')
+                err.status = 401
+                return next(err)
+            } else {
+                return next()
+            }
+        }
+    })
 }
 module.exports = {
     isAuth
